50ea0ee20738bbdb994e2cb0f0b868693e3d98dc
yxzh <cstrikest@gmail.com>
2019-05-08 14:59:09 +0900

    debug

---
+++ AI_core_logic_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+__author__ = "Yxzh"
+
+import Snakey_core
+import AI_core_logic
+import time
+
+
+print("Start.")
+G = Snakey_core.Snakey(bomb = 0)
+a = 10000
+i = a
+max = 0
+sum = 0
+start = time.time()
+while True:
+	G.next(AI_core_logic.get_next_direction(G.pos, G.food_pos, G.snakes))
+	if G.deadflag:
+		ate = G.ate
+		print(ate)
+		sum += ate
+		if ate > max:
+			max = ate
+		G.reset()
+		i -= 1
+		if i is 0:
+			break
+print("\nmax ate: ", max)
+print("average ate:", sum / a)
+end = time.time()
+print("use {} seconds, {} seconds per game".format(end - start, (end - start) / a))
--- Snakey_core.py
+++ Snakey_core.py
@@ -10,8 +10,8 @@ PLAYGROUND_HEIGHT = 200  # 游戏区域大小
 
 class Snakey(object):
 	
-	def __init__(self, bomb = True):
-		self.allow_bomb = bomb
+	def __init__(self, bomb = 1):
+		self.bomb_number = bomb
 		self.pos = [0, 0]  # 蛇头位置
 		self.direction = "S"  # 上一步蛇头方向
 		self.snakes = [(0, 0)] * 2  # 蛇数组
@@ -69,17 +69,16 @@ class Snakey(object):
 		
 		del (self.snakes[0])  # 删除蛇数组顶
 		
-		if self.pos in self.snakes:  # 自身碰撞检测
-			print("z")
+		if (self.pos[0], self.pos[1]) in self.snakes:  # 自身碰撞检测
 			self.deadflag = True  # 触发死亡判定
 		
 		self.snakes.append((self.pos[0], self.pos[1]))  # 推入蛇数组底
 		
-		while (len(self.bombs) < 11) and self.allow_bomb:  # 刷新炸弹
-			bomb_pos = [randint(0, PLAYGROUND_WIDTH / 10 - 1) * 10, randint(0, PLAYGROUND_HEIGHT / 10 - 1) * 10]
+		while len(self.bombs) < self.bomb_number:  # 刷新炸弹
+			bomb_pos = (randint(0, PLAYGROUND_WIDTH / 10 - 1) * 10, randint(0, PLAYGROUND_HEIGHT / 10 - 1) * 10)
 			while (bomb_pos in self.snakes) or (self.food_pos[0] == bomb_pos[0] or self.food_pos[1] == bomb_pos[1]) or (
 							  bomb_pos in self.bombs):  # 避免与蛇和食物和其他炸弹重叠刷新
-				bomb_pos = [randint(0, PLAYGROUND_WIDTH / 10 - 1) * 10, randint(0, PLAYGROUND_HEIGHT / 10 - 1) * 10]
+				bomb_pos = (randint(0, PLAYGROUND_WIDTH / 10 - 1) * 10, randint(0, PLAYGROUND_HEIGHT / 10 - 1) * 10)
 			self.bombs.append(bomb_pos)
 		
 		if not self.isfood:  # 根据食物判定刷新食物
@@ -89,12 +88,11 @@ class Snakey(object):
 				                 randint(0, PLAYGROUND_HEIGHT / 10 - 1) * 10]
 			self.isfood = True
 		
-		if self.pos in self.bombs and self.allow_bomb:  # 炸弹碰撞检测
-			print("b")
+		if (self.pos[0], self.pos[1]) in self.bombs:  # 炸弹碰撞检测
 			self.deadflag = True
 		
 		if self.pos == self.food_pos:  # 吃食物事件
-			self.snakes.append([int(self.pos[0]), int(self.pos[1])])  # 将当前位置推入蛇数组
+			self.snakes.append((self.pos[0], self.pos[1]))  # 将当前位置推入蛇数组
 			self.ate += 1
 			self.isfood = False
 		
