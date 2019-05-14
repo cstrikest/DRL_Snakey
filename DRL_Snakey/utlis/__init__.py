2bf754543e1a1c60b09d13557add24c7eb5c8121
yxzh <cstrikest@gmail.com>
2019-05-14 17:35:38 +0900

    更新演示文件部分

---
+++ DRL_Snakey/utlis/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+__author__ = "Yxzh"
+
+from .training import Train
---
+++ DRL_Snakey/utlis/training.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+__author__ = "Yxzh"
+
+
+import tensorflow as tf
+import numpy as np
+
+from tensorflow import keras
+
+class Train(object):
+	pass
